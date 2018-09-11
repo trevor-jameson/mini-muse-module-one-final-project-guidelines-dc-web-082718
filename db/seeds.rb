2f0f67e8ecae2b99d54494e76d93ab5cd3aa41ed
Trevor Jameson <github email address>
2018-09-11 18:09:28 -0400

    refactored seeds.rb import methods and added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,36 @@
 class ImportCsv
 
+  @file = File.open("db/albumlist.csv")
+
   def self.insert_artist_data
     # May need to drop all tables prior to added to DB
-    file = File.open("db/albumlist.csv")
-    fields_to_insert =  %w{ Number Artist }
+    # Shovels what we want to search for in the array
     rows_to_insert = []
-    
-    CSV.foreach(file, :headers => true) do |row|
+
+    # Parse the CSV file for all Artist object attributes and return array with elements of attributes
+    CSV.foreach(@file, :headers => true) do |row|
       row_hash = row.to_hash
-      output_hash = {}
-      output_hash[:id] = row_hash["Number"]
-      output_hash[:name] = row_hash["Artist"]
-
+      output_hash = {id: row_hash["Number"], name: row_hash["Artist"]}
       rows_to_insert << output_hash
-      rows_to_insert.uniq! {|element| element[:name]}
     end
+    # Delete Artist elements with the same name
+    rows_to_insert.uniq! {|element| element[:name]}
 
+    # Create Artist instance for each elements in row_to_insert and add it to database
     Artist.import(rows_to_insert)
   end
 
   def self.insert_genre_data
     # May need to drop all tables prior to added to DB
-    file = File.open("db/albumlist.csv")
-    fields_to_insert =  %w{ Number Genre }
     rows_to_insert = []
 
-    CSV.foreach(file, :headers => true) do |row|
+    #
+    CSV.foreach(@file, :headers => true) do |row|
       row_hash = row.to_hash
-
-      output_hash = {}
-      output_hash[:id] = row_hash["Number"]
-      output_hash[:name] = row_hash["Genre"]
-
+      output_hash = {id: row_hash["Number"], name: row_hash["Genre"]}
       rows_to_insert << output_hash
-      rows_to_insert.uniq! {|element| element[:name]}
     end
+    rows_to_insert.uniq! {|element| element[:name]}
 
     Genre.import(rows_to_insert)
   end
