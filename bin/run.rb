e095e9723295c401ff6048ccb60ce14c94451be6
Trevor Jameson <github email address>
2018-09-12 09:53:52 -0400

    Added CLI file and ascii art. Building welcome page.

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,3 @@
 require_relative '../config/environment'
 
-# file = File.open("db/albumlist.csv")
-# CSV.foreach(file, :headers => true) do |row|
-#   binding.pry
-#   Artist.create(:name => row["Artist"])
-# end
-
-puts "HELLO WORLD"
+Artscii.welcome
