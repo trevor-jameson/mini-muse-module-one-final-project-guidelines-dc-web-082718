9ad7f0567d87fd8598af7f245c9fc796286db7d4
github username <github email address>
2018-09-12 09:56:20 -0400

    can successfully  import from CSV to db

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '../db/greatest_albums.db')
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/greatest_albums.db')
 require_all 'lib'
