751047aee40b2440b948fc97997c4723ad8e4161
github username <github email address>
2018-09-13 22:05:01 -0400

    Brian: ready as of Thurs night

--- lib/models/artist.rb
+++ lib/models/artist.rb
@@ -12,12 +12,12 @@ class Artist < ActiveRecord::Base
     # get the artist_id of the artist
     artist_id = self.where("name = ?", artist_name).pluck(:id)
     # get the top 3 albums of this artist
-    puts Album.limit(3).where("artist_id = ?", artist_id).pluck(:name)
+    Album.limit(3).where("artist_id = ?", artist_id).pluck(:name)
   end
 
   def self.most_popular_album(artist_name)
     artist_id = self.where("name = ?", artist_name).pluck(:id)
-    puts Album.limit(1).where("artist_id = ?", artist_id).pluck(:name)
+    Album.limit(1).where("artist_id = ?", artist_id).pluck(:name)
   end
 
   def self.genres_part_of(artist_name)
@@ -29,7 +29,7 @@ class Artist < ActiveRecord::Base
     genre_names = genre_ids.map do |genre_id|
       Genre.where("id = ?", genre_id).pluck(:name)
     end
-    puts genre_names.uniq
+    genre_names.uniq
   end
 
 end
